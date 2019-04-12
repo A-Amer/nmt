e4319bd0e2e31b5e356ded79c59f91b4c9276a4c
A-Amer <amira.a.amer@gmail.com>
2019-04-12 15:24:44 +0200

    Add files via upload

--- onmt/encoders/rnn_encoder.py
+++ onmt/encoders/rnn_encoder.py
@@ -23,8 +23,7 @@ class RNNEncoder(EncoderBase):
     """
 
     def __init__(self, rnn_type, bidirectional, num_layers,
-                 hidden_size, dropout=0.0, embeddings=None,
-                 use_bridge=False):
+                 hidden_size, dropout=0.0, embeddings=None):
         super(RNNEncoder, self).__init__()
         assert embeddings is not None
 
@@ -41,12 +40,7 @@ class RNNEncoder(EncoderBase):
                         dropout=dropout,
                         bidirectional=bidirectional)
 
-        # Initialize the bridge layer
-        self.use_bridge = use_bridge
-        if self.use_bridge:
-            self._initialize_bridge(rnn_type,
-                                    hidden_size,
-                                    num_layers)
+       
 
     @classmethod
     def from_opt(cls, opt, embeddings):
@@ -57,8 +51,7 @@ class RNNEncoder(EncoderBase):
             opt.enc_layers,
             opt.enc_rnn_size,
             opt.dropout,
-            embeddings,
-            opt.bridge)
+            embeddings)
 
     def forward(self, src, lengths=None):
         """See :func:`EncoderBase.forward()`"""
@@ -78,38 +71,4 @@ class RNNEncoder(EncoderBase):
         if lengths is not None and not self.no_pack_padded_seq:
             memory_bank = unpack(memory_bank)[0]
 
-        if self.use_bridge:
-            encoder_final = self._bridge(encoder_final)
         return encoder_final, memory_bank, lengths
-
-    def _initialize_bridge(self, rnn_type,
-                           hidden_size,
-                           num_layers):
-
-        # LSTM has hidden and cell state, other only one
-        number_of_states = 2 if rnn_type == "LSTM" else 1
-        # Total number of states
-        self.total_hidden_dim = hidden_size * num_layers
-
-        # Build a linear layer for each
-        self.bridge = nn.ModuleList([nn.Linear(self.total_hidden_dim,
-                                               self.total_hidden_dim,
-                                               bias=True)
-                                     for _ in range(number_of_states)])
-
-    def _bridge(self, hidden):
-        """Forward hidden state through bridge."""
-        def bottle_hidden(linear, states):
-            """
-            Transform from 3D to 2D, apply linear and return initial size
-            """
-            size = states.size()
-            result = linear(states.view(-1, self.total_hidden_dim))
-            return F.relu(result).view(size)
-
-        if isinstance(hidden, tuple):  # LSTM
-            outs = tuple([bottle_hidden(layer, hidden[ix])
-                          for ix, layer in enumerate(self.bridge)])
-        else:
-            outs = bottle_hidden(self.bridge[0], hidden)
-        return outs
