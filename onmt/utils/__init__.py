e95a15f544a83697d3a00ce963ea931990a591db
A-Amer <amira.a.amer@gmail.com>
2019-04-13 08:36:57 +0200

    Update __init__.py

--- onmt/utils/__init__.py
+++ onmt/utils/__init__.py
@@ -4,6 +4,7 @@ from onmt.utils.report_manager import ReportMgr, build_report_manager
 from onmt.utils.statistics import Statistics
 from onmt.utils.optimizers import Optimizer
 from onmt.utils.earlystopping import EarlyStopping, scorers_from_opts
+import onmt.utils.loss
 
 __all__ = ["split_corpus", "aeq", "use_gpu", "set_random_seed", "ReportMgr",
            "build_report_manager", "Statistics",
