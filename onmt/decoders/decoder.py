b468c436720b58796b6a4f38c903bc60c9148b31
A-Amer <amira.a.amer@gmail.com>
2019-04-14 22:56:08 +0200

    Update decoder.py

--- onmt/decoders/decoder.py
+++ onmt/decoders/decoder.py
@@ -138,7 +138,6 @@ class RNNDecoderBase(DecoderBase):
 
         # Init the input feed.
         batch_size = self.state["hidden"][0].size(1)
-        print(memory_bank.size())
         h_size = (batch_size, self.hidden_size)
         self.state["input_feed"] = \
             self.state["hidden"][0].data.new(*h_size).zero_().unsqueeze(0)
