09b438989220c2147e730cf8c871aad500a7c6e5
A-Amer <amira.a.amer@gmail.com>
2019-04-15 19:41:10 +0200

    Update __init__.py

--- onmt/translate/__init__.py
+++ onmt/translate/__init__.py
@@ -6,10 +6,9 @@ from onmt.translate.beam_search import BeamSearch
 from onmt.translate.decode_strategy import DecodeStrategy
 from onmt.translate.random_sampling import RandomSampling
 from onmt.translate.penalties import PenaltyBuilder
-from onmt.translate.translation_server import TranslationServer, \
-    ServerModelError
+
 
 __all__ = ['Translator', 'Translation', 'Beam', 'BeamSearch',
            'GNMTGlobalScorer', 'TranslationBuilder',
-           'PenaltyBuilder', 'TranslationServer', 'ServerModelError',
+           'PenaltyBuilder', 
            "DecodeStrategy", "RandomSampling"]
