ff15abfa195febc4c9aacf9a69b889d9ea85be04
A-Amer <amira.a.amer@gmail.com>
2019-04-15 20:17:29 +0200

    Update translator.py

--- onmt/translate/translator.py
+++ onmt/translate/translator.py
@@ -15,7 +15,34 @@ import onmt.inputters as inputters
 from onmt.translate.beam_search import BeamSearch
 from onmt.translate.random_sampling import RandomSampling
 from onmt.utils.misc import tile, set_random_seed
-from onmt.modules.copy_generator import collapse_copy_scores
+
+def collapse_copy_scores(scores, batch, tgt_vocab, src_vocabs,
+                         batch_dim=1, batch_offset=None):
+    """
+    Given scores from an expanded dictionary
+    corresponeding to a batch, sums together copies,
+    with a dictionary word when it is ambiguous.
+    """
+    offset = len(tgt_vocab)
+    for b in range(scores.size(batch_dim)):
+        blank = []
+        fill = []
+        batch_id = batch_offset[b] if batch_offset is not None else b
+        index = batch.indices.data[batch_id]
+        src_vocab = src_vocabs[index]
+        for i in range(1, len(src_vocab)):
+            sw = src_vocab.itos[i]
+            ti = tgt_vocab.stoi[sw]
+            if ti != 0:
+                blank.append(offset + i)
+                fill.append(ti)
+        if blank:
+            blank = torch.Tensor(blank).type_as(batch.indices.data)
+            fill = torch.Tensor(fill).type_as(batch.indices.data)
+            score = scores[:, b] if batch_dim == 1 else scores[b]
+            score.index_add_(1, fill, score.index_select(1, blank))
+            score.index_fill_(1, blank, 1e-10)
+    return scores
 
 
 def build_translator(opt, report_score=True, logger=None, out_file=None):
