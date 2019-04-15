9b65bd318e7463a47a8803ef88c9846795105f0b
A-Amer <amira.a.amer@gmail.com>
2019-04-15 09:50:29 +0200

    Update __init__.py

--- onmt/encoders/__init__.py
+++ onmt/encoders/__init__.py
@@ -2,10 +2,11 @@
 from onmt.encoders.encoder import EncoderBase
 from onmt.encoders.transformer import TransformerEncoder
 from onmt.encoders.rnn_encoder import RNNEncoder
+from onmt.encoders.residual_encoder import ResidualEncoder
 
 
 
-str2enc = {"rnn": RNNEncoder, "brnn": RNNEncoder,"gnmt":GoogleEncoder,
+str2enc = {"rnn": RNNEncoder, "brnn": RNNEncoder,"res":ResidualEncoder,
            "transformer": TransformerEncoder}
 
-__all__ = ["EncoderBase", "TransformerEncoder", "RNNEncoder", "str2enc"]
+__all__ = ["EncoderBase", "TransformerEncoder", "RNNEncoder","ResidualEncoder", "str2enc"]
