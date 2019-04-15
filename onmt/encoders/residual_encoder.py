d239d0cc004224b7019433389a42fc17958a7f11
A-Amer <amira.a.amer@gmail.com>
2019-04-15 11:10:54 +0200

    Update residual_encoder.py

--- onmt/encoders/residual_encoder.py
+++ onmt/encoders/residual_encoder.py
@@ -106,7 +106,7 @@ class ResidualEncoder(EncoderBase):
             memory_bank = self.dropout(memory_bank)
             memory_bank, encoder_final = self.layers[i](memory_bank)
             memory_bank = memory_bank+ residual
-        print(encoder_final.size())
+        print(encoder_final[0].size())
         print(memory_bank.size())
 
         return encoder_final, memory_bank, lengths
