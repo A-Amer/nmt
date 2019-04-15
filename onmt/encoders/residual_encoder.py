e474b8d25ab433c912558017d345e28757c81a43
A-Amer <amira.a.amer@gmail.com>
2019-04-15 11:25:09 +0200

    Update residual_encoder.py

--- onmt/encoders/residual_encoder.py
+++ onmt/encoders/residual_encoder.py
@@ -1,6 +1,6 @@
 """Define RNN-based encoders."""
 import torch.nn as nn
-
+import torch
 from torch.nn.utils.rnn import pack_padded_sequence as pack
 from torch.nn.utils.rnn import pad_packed_sequence as unpack
 
