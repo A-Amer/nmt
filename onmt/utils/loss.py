0a3f7df4a9941a7986acb6ce383c106901e1774b
A-Amer <amira.a.amer@gmail.com>
2019-04-17 11:54:00 +0200

    Update loss.py

--- onmt/utils/loss.py
+++ onmt/utils/loss.py
@@ -143,10 +143,6 @@ class LossComputeBase(nn.Module):
         non_padding = target.ne(self.padding_idx)
         num_correct = pred.eq(target).masked_select(non_padding).sum().item()
         num_non_padding = non_padding.sum().item()
-        #if valid:
-          #print(pred.masked_select(non_padding).size())
-          #print(target.masked_select(non_padding).size())
-
         return onmt.utils.Statistics(loss.item(), num_non_padding, num_correct)
 
     def _bottle(self, _v):
