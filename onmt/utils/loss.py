c1917140ecace266dfb1e09df7651079a9078fb2
A-Amer <amira.a.amer@gmail.com>
2019-04-22 11:45:57 +0200

    Update loss.py

--- onmt/utils/loss.py
+++ onmt/utils/loss.py
@@ -205,7 +205,7 @@ class NMTLossCompute(LossComputeBase):
             loss_data = loss.data.clone()
         elif prediction_type == "sample":
             inputs_t = target[0, :]
-            continue_gen = (inputs_t.ne(self.eos_token) * inputs_t.ne(0))
+            continue_gen = (inputs_t.ne(self.tgt_vocab.stoi[self.eos_token]) * inputs_t.ne(self.padding_idx))
             tgt_t = continue_gen.long()
             gtruth=tgt_t.view(-1)
             d = torch.distributions.Categorical(
